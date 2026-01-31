d9e178c60e44805e7093b77baf1a1f1df20c94bf
dartMo10 <dartmore10@yahoo.com>
2026-01-30 18:04:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ if prompt := st.chat_input("Chat about the program of Alcoholics Anonymous ...")
                     sources_text += f"Metadata: {node.node.metadata}\n"
             
             response = openai_client.chat.completions.create(
-                model="cohere/command-r-plus-08-2024",
+                model="mistralai/mistral-8x-instruct",
                 messages=[
                     {"role": "system", "content": f"""You are a consultant on the Alcoholics Anonymous program. You are to aid the user while following these rules strictly:
 
